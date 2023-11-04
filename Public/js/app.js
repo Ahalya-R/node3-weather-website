54ef9e72c179d90173de35f0d112f9ba8aaee181
Ahalya-R <rathinakumarahalya@gmail.com>
2023-11-04 21:19:14 +0530

    feat: upate import config

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,23 +1,23 @@
-const config = require('config')
+import config from "config";
 const weatherForm = document.querySelector('form')
-const search=document.querySelector('input')
+const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
 const message2 = document.querySelector('#message-2')
 //message1.textContent='From JavaScript'
 
-weatherForm.addEventListener('submit', (e)=>{
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const location=search.value
-    message1.textContent='..Loading'
-    message2.textContent=''
-    fetch(`${config.get("url")}/weather?address=`+location).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            message1.textContent=data.error
-        }else{
-            message1.textContent=data.location
-            message2.textContent=data.forecast
-        }
+    const location = search.value
+    message1.textContent = '..Loading'
+    message2.textContent = ''
+    fetch(`${config.get("url")}/weather?address=` + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error
+            } else {
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+            }
+        })
     })
-})  
 })
